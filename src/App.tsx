32660acea7c94f1d92ea361bcc606d28a7ee1354
takato <tk1592.sayusayu@gmail.com>
2024-11-20 23:26:43 +0900

    雛形ページ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Report from './pages/Report';
+import NoMatch from './pages/NoMatch';
+import AppLayout from './component/AppLayout';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />}></Route>
+          <Route path='/report' element={<Report />}></Route>
+          <Route path='/*' element={<NoMatch />}></Route>
+          </Route>
+      </Routes>
+    </Router>
+
   );
 }
 
